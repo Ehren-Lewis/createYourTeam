4cb50213a112a78999e27cc09575f62608290af8
Ehren-Lewis <89164935+Ehren-Lewis@users.noreply.github.com>
2022-08-28 18:43:03 -0500

    Tested all other prompts, they all work. Added in a style subdirectory just in case, created the index.html to append to perhaps (may do it instead by creating an index.html and writing to it fresh), working on creating JQuery to show the results

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import readline from 'readline-sync';
 import inquirer from 'inquirer'
-import { react } from '@babel/types';
 class Person {
     constructor(name, ID, email) {
         this.name = name;
         this.ID = ID,
         this.email = email;
+    }
+    displayInformation () {
+        console.log(`
+        name: ${this.name}
+        ID: ${this.ID}
+        email: ${this.email}`)
     }
 }
 
@@ -13,20 +18,24 @@ class Manager extends Person {
     constructor(name, ID, email, number) {
         super(name, ID, email);
         this.number = number;
+        this.type = "Manager";
     }
 
-    displayInformation () {
-        console.log(`
-        name: ${this.name}
-        ID: ${this.ID}
-        email: ${this.email}
-        number: ${this.number}`)
+    displayInformation() {
+        super.displayInformation();
+        console.log(        `number: ${this.number}`);
     }
+
 }
 class Engineer extends Person {
     constructor(name, ID, email, gitHub) {
         super(name, ID, email);
         this.gitHub = gitHub;
+        this.type = "Engineer";
+    }
+    displayInformation() {
+        super.displayInformation();
+        console.log(`       gitHub: ${this.gitHub}`);
     }
 }
 
@@ -34,19 +43,16 @@ class Intern extends Person {
     constructor(name, ID, email, school) {
         super(name, ID, email);
         this.school = school;
+        this.type = "Intern";
+    }
+    displayInformation() {
+        super.displayInformation();
+        console.log(`       school: ${this.school}`);
     }
 }
 
 // On load, enter manager then team manager.
 // then prompt for engineer or associaate or finish
-
-
-
-// const promptFactory = () => {
-    
-// }
-
-
 
 const finalEmployees = [];
 
@@ -59,43 +65,48 @@ const createEngineer = () => {
         "type": "input"
     },
     {
-        "name": "engineerID",
-        "message": "What is the engineer's ID?",
-        "type": "input"
+    "name": "engineerID",
+    "message": "What is the engineer's ID?",
+    "type": "input"
     },   
     {
-        "name": "engineerEmail",
-        "message": "What is the engineer's email?",
-        "type": "input"
+    "name": "engineerEmail",
+    "message": "What is the engineer's email?",
+    "type": "input"
     },   
     {
-        "name": "engineerGitHub",
-        "message": "What is the engineer's GitHub?",
-        "type": "input"
+    "name": "engineerGitHub",
+    "message": "What is the engineer's GitHub?",
+    "type": "input"
     },
     {
-        "name":"whichType",
-        "message": "Would you like to input an engineer, intern, or exit?",
-        "type": "list",
-        "choices": ["Engineer", "Intern", "Quit"]
+    "name":"whichType",
+    "message": "Would you like to input an engineer, intern, or exit?",
+    "type": "list",
+    "choices": ["Engineer", "Intern", "Quit"]
     }
-    ]).then( (answers) => {
-        const engineer = new Engineer(
-            answers.engineerName, answers.engineerID, 
-            answers.engineerEmail, answers.engineerGitHub);
-        
-        finalEmployees.push(engineer);
-        switch (answers.whichType) {
-            case "Engineer": 
-            createEngineer(finalEmployees);
-            break;
-            case "Intern":
-                createIntern(finalEmployees);
-            break;
-            case "Quit":
-                onQuit(finalEmployees)
-        }
-    })
+
+]).then( (answers) => {
+    const engineer = new Engineer(
+        answers.engineerName, answers.engineerID, 
+        answers.engineerEmail, answers.engineerGitHub);
+    
+    finalEmployees.push(engineer);
+
+    switch (answers.whichType) {
+        case "Engineer": 
+        createEngineer(finalEmployees);
+        break;
+        case "Intern":
+            createIntern(finalEmployees);
+        break;
+        case "Quit":
+            onQuit(finalEmployees)
+    }
+
+}).catch( (err) => {
+    console.log(err);
+})
 }
 
 const createIntern = () => {
@@ -127,11 +138,13 @@ const createIntern = () => {
         "type": "list",
         "choices": ["Engineer", "Intern", "Quit"]
     }
+
 ]).then( (answers) => {
     const intern = new Intern(answers.internName, 
         answers.internID, answers.internEmail, answers.internSchool);
     
     finalEmployees.push(intern);
+    intern.displayInformation();
     switch (answers.whichType) {
         case "Engineer": 
         createEngineer(finalEmployees);
@@ -148,13 +161,50 @@ const createIntern = () => {
 })
 }
 
-const onQuit = (employees) => {
+const onQuit = () => {
+
+    const numberOfRows = finalEmployees.length // 3 + 1;
+    const container = $(".container")
+    const row = $("<div class='row'>")
+
+    for (let i = 0; i < finalEmployees.length; i++ ) {
+        if ( (i + +1 ) %  3 == 0) {
+            container.append(row);
+        }
+
+        let roleLi = ``;
+
+        switch (finalEmployees[i].type) {
+            case "Manager":
+                roleLi = `<li class='list-group-item>Number: ${finalEmployees[i].number}</li>`
+                break;
+            case "Engineer":
+                roleLi = `<li class='list-group-item>Number: ${finalEmployees[i].gitHub}</li>`
+                break;
+            case "Intern":
+                roleLi = `<li class='list-group-item>Number: ${finalEmployees[i].school}</li>`
+                break;
+        }
+
+        const card = $("<div class='card'></div>");
+        const cardBody = $("<div class='card-body'></div");
+        cardBody.append(`<h5 class='card-title'>${finalEmployees[i].name}</div>`);
+        cardBody.apend(`<h6 class='card-subtitle'>${finalEmployees[i].type}</div>`);
+        cardBody.append(
+`<ul class='list-group'>
+    <li class='list-group-item'>ID: </li>
+    <li class='list-group-item'>Email: </li>
+    ${roleLi}
+</ul>`)
+        card.append(cardBody);
+    }
 
 }
 
 const loadingQuestion = inquirer.createPromptModule();
 
 console.log("Welcome to your team Builder!")
+
 loadingQuestion([
     {
         "name": "managerName",
@@ -189,7 +239,6 @@ loadingQuestion([
     
     finalEmployees.push(teamManager);
     teamManager.displayInformation();
-    // return;
     
     switch (answers.whichType) {
         case "Engineer": 
